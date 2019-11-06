b2ee5b8c35d08c6c08d021c90021bfb83baf0e9e
guillaume <gueyome@hotmail.fr>
2019-11-06 19:34:43 +0100

    stripe

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
   resources :users
+  resources :charges
   root 'events#index'
 end
