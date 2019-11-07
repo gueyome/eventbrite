2c5759670a18f31073fcba1e540c567589fe769c
guillaume <gueyome@hotmail.fr>
2019-11-07 21:17:39 +0100

    call back

--- app/controllers/event_pictures_controller.rb
+++ app/controllers/event_pictures_controller.rb
@@ -1,7 +1,19 @@
 class EventPicturesController < ApplicationController
+  before_action :user_admin?, only: [:create]
+  
   def create
     @event = Event.find(params[:event_id])
     @event.event_picture.attach(params[:event_picture])
     redirect_to(event_path(@event))
   end
+
+  private
+
+  def user_admin?
+    unless Event.find(params[:event_id]).admin_id == current_user.id
+      flash[:danger] = "Vous n'êtes pas le créateur de cet évènement"
+      redirect_to event_path(params[:event_id])
+    end
+  end
+
 end
