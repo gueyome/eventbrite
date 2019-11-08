2e94ac206626faed9466b20e4d7d9a0a94478991
guillaume <gueyome@hotmail.fr>
2019-11-08 21:00:41 +0100

    back end qq doutes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, :good_user_connected, only: [:show]
 
+  
+  
   def show
     puts params
     puts a = params[:id]
     @user = User.find(a)
   end
+
+  def destroy
+    a = params[:id]
+    @user = User.find(a)
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
 
   private
 
